713bcd9c01e41ab727df1ead3f927d36ba96518c
Medeirosvdd <medeirosmurilo589@gmail.com>
2024-12-09 18:43:47 -0300

    primeiro commit

---
+++ JS/Login.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.Login_name'); 
+const button = document.querySelector('.Login_botão');
+const form = document.querySelector('.Login-Form')
+
+const validateInput = ({ target }) => {
+  if (target.value.length > 2) {
+    button.removeAttribute('disabled');
+    return;
+  }
+    button.setAttribute('disabled', ''); 
+
+};
+
+const handleSubmit = (event) => {
+ event.preventDefault();
+ 
+ localStorage.setItem('palyer', input.value);
+ window.location = 'jogo.html'
+}
+
+input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
